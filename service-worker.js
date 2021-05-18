cc0c461b446e15beafbbc617817b0f184d7ff2eb
Bulaev Denis <bulaevdenis7@gmail.com>
2021-05-18 23:40:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.3c42173a83d0d063744180c74b2ef16e.js"
 );
 
 self.addEventListener('message', (event) => {
